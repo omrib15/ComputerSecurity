aba08bea9a6f111753d65f35a6b4d2b4d5103d1c
Omri Basch <omrib15@gmail.com>
2018-06-01 17:12:43 +0300

    some name changes

--- src/client/HttpDownloadUtility.java
+++ src/client/HttpDownloadUtility.java
@@ -69,6 +69,7 @@ public class HttpDownloadUtility {
             inputStream.close();
             
             System.out.println("File downloaded");
+            
             //notify successful download
             JOptionPane.showMessageDialog(null,
                     "File has been downloaded successfully!", "Message",
